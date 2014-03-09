abad8d59b5736d36b794278b5737c4e2a7f417a7
Stephan Toggweiler <stephan.toggweiler@me.com>
2014-03-09 15:50:48 +0100

    change home_screen to formotion screen

--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -1,10 +1,25 @@
-class HomeScreen < PM::Screen
+class HomeScreen < PM::FormotionScreen
   title "Home"
 
   def on_load
   end
 
   def will_appear
+  end
+  
+  def table_data
+    {
+      sections: [{
+        title: "Item",
+        rows: [{
+          title: "Name",
+          key: :name,
+          type: :string,
+          placeholder: "text here",
+          value: "Testtext"
+          }]
+      }]
+    }
   end
 
 end
